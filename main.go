bdf4d684ee5c914213299c11e397336cc2dff31b
fujimoto kyosuke <kyoro.f@gmail.com>
2020-01-05 02:11:40 +0900

    add option: show legend

--- main.go
+++ main.go
@@ -8,23 +8,58 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const (
+	appName  = "crass"
+	appUsage = "growing grass on CUI"
+
+	flagLegend = "legend"
+)
+
+var flags = []cli.Flag{
+	&cli.BoolFlag{
+		Name:     flagLegend,
+		Aliases:  []string{"l"},
+		Usage:    "show legend",
+		Required: false,
+		Value:    false,
+	},
+}
+
+type options struct {
+	legend bool
+}
+
+func (o *options) showLegend() bool {
+	return o.legend
+}
+
+func parseFlags(c *cli.Context) *options {
+	return &options{
+		legend: c.Bool(flagLegend),
+	}
+}
+
 func action(c *cli.Context) error {
 	if c.NArg() == 0 {
 		return nil // TODO: error
 	}
-	grasses, err := grass.Mow(c.Args().Get(0))
+	id := c.Args().Get(0)
+	opt := parseFlags(c)
+
+	grasses, err := grass.Mow(id)
 	if err != nil {
 		return err
 	}
-	printGrasses(grasses)
+	printGrasses(grasses, opt)
 	return nil
 }
 
 func main() {
 	app := &cli.App{
-		Name:   "crass",
-		Usage:  "growing grass on CUI",
+		Name:   appName,
+		Usage:  appUsage,
 		Action: action,
+		Flags:  flags,
 	}
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
